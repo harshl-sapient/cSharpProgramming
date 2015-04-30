bb955b0eb2d8e982f06bf1b37070da362c79f81a
harsh18 <harsh.lamba181189@gmail.com>
2015-04-30 16:44:31 +0530

    commiting calculation using private static method

---
+++ CalculatorAddition/CalculatorAddition/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorAddition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              int resultToDisplay = AdditionNumber(2, 2);
+            Console.WriteLine(resultToDisplay);
+            Console.ReadKey();
+        }
+        //Method to Add data and return it
+        private static int AdditionNumber(int NumberOne, int NumberTwo) 
+        {
+            int calculationResult;
+            calculationResult = NumberOne + NumberTwo;
+            return calculationResult;
+        }
+
+    }
+}
