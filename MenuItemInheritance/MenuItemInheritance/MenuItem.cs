0030d46e43145a78f6523755a88687be7d9a530f
harsh18 <harsh.lamba181189@gmail.com>
2015-05-01 20:46:10 +0530

    Inheritance

---
+++ MenuItemInheritance/MenuItemInheritance/MenuItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuItemInheritance
+{
+    class MenuItem
+    {
+        public string name;
+        public string description;
+        public double price;
+    }
+}
